97eb331e742411c9ec9835c74309cfd837a57846
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-09 16:26:58 +0530

    nested grid used

--- src/components/POC.js
+++ src/components/POC.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { TextField, Button, Grid, RadioGroup, FormControlLabel, FormHelperText, Radio } from '@mui/material';
+import { TextField, Button, Grid, RadioGroup, FormControlLabel, FormHelperText, Radio, Typography } from '@mui/material';
 
 function POC() {
+
   const { register, handleSubmit, formState: { errors }, watch } = useForm();
 
   const onSubmit = (data) => {
@@ -11,60 +12,88 @@ function POC() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid container direction="column" spacing={4} border='1px solid' justifyContent="center"
-        alignItems="center">
-        <Grid item>
-          <TextField
-            label="First Name"
-            size="small"
-            {...register('firstName', { required: true })}
-            error={!!errors.firstName}
-            helperText={errors.firstName ? 'First Name is required' : ''}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            label="Last Name"
-            size="small"
-            {...register('lastName', { required: true })}
-            error={!!errors.lastName}
-            helperText={errors.lastName ? 'Last Name is required' : ''}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            size="small"
-            label="Email"
-            {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-            error={!!errors.email}
-            helperText={errors.email ? 'Valid email is required' : ''}
-          />
-        </Grid>
-        <Grid item>
-          <RadioGroup row aria-label="gender" name="gender" >
-            <FormControlLabel
-              value="male"
-              control={<Radio {...register('gender', { required: 'Gender is required' })} />}
-              label="Male"
+      <Grid container lg={12} spacing={4} border='1px solid' justifyContent="center" sx={{ m: 2 }}
+      >
+        <Grid container lg={7} justifyContent="space-between" item direction="row">
+          <Grid item>
+            <Typography variant="h6" >
+              First Name:
+            </Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              label="First Name"
+              size="small"
+              {...register('firstName', { required: true })}
+              error={!!errors.firstName}
+              helperText={errors.firstName ? 'First Name is required' : ''}
             />
-            <FormControlLabel
-              value="female"
-              control={<Radio {...register('gender', { required: 'Gender is required' })} />}
-              label="Female"
-            />
-            <FormControlLabel
-              value="other"
-              control={<Radio {...register('gender', { required: 'Gender is required' })} />}
-              label="Other"
-            />
-          </RadioGroup>
-          {errors.gender && (
-            <FormHelperText error>
-              {errors.gender.message}
-            </FormHelperText>
-          )}
+          </Grid>
         </Grid>
-        <Grid item>
+        <Grid container lg={7} justifyContent="space-between" item direction="row">
+          <Grid item>
+            <Typography variant="h6" >
+              Last Name:
+            </Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Last Name"
+              size="small"
+              {...register('lastName', { required: true })}
+              error={!!errors.lastName}
+              helperText={errors.lastName ? 'Last Name is required' : ''}
+            />
+          </Grid>
+        </Grid>
+        <Grid container lg={7} justifyContent="space-between" item direction="row">
+          <Grid item>
+            <Typography variant="h6" >
+              Email:
+            </Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              size="small"
+              label="Email"
+              {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+              error={!!errors.email}
+              helperText={errors.email ? 'Valid email is required' : ''}
+            />
+          </Grid>
+        </Grid>
+        <Grid container lg={7} justifyContent="space-between" item direction="row">
+          <Grid item>
+            <Typography variant="h6" >
+              Gender:
+            </Typography>
+          </Grid>
+          <Grid item>
+            <RadioGroup row aria-label="gender" name="gender" >
+              <FormControlLabel
+                value="male"
+                control={<Radio {...register('gender', { required: 'Gender is required' })} />}
+                label="Male"
+              />
+              <FormControlLabel
+                value="female"
+                control={<Radio {...register('gender', { required: 'Gender is required' })} />}
+                label="Female"
+              />
+              <FormControlLabel
+                value="other"
+                control={<Radio {...register('gender', { required: 'Gender is required' })} />}
+                label="Other"
+              />
+            </RadioGroup>
+            {errors.gender && (
+              <FormHelperText error>
+                {errors.gender.message}
+              </FormHelperText>
+            )}
+          </Grid>
+        </Grid>
+        <Grid item lg={7}>
           <Button type="submit">Submit</Button>
         </Grid>
       </Grid>
