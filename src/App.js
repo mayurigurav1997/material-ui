aa2b40999d2f71584e19114d6c92b28e88a84622
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-25 12:18:33 +0530

    tooltip

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import { MuiAvatar } from "./components/MuiAvatar";
 import { MuiBadge } from "./components/MuiBadge";
 import { MuiList } from "./components/MuiList";
 import { MuiChip } from "./components/MuiChip";
+import { MuiTooltip } from "./components/MuiTooltip";
 
 
 function App() {
@@ -55,7 +56,8 @@ function App() {
       {/* <MuiAvatar /> */}
       {/* <MuiBadge /> */}
       {/* <MuiList /> */}
-      <MuiChip />
+      {/* <MuiChip /> */}
+      <MuiTooltip />
 
     </div>
   );
