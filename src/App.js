4c0520b8a684f8d4c4c6b9dd30a47f76171b4842
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-25 13:14:06 +0530

    Alert in Mui

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import { MuiList } from "./components/MuiList";
 import { MuiChip } from "./components/MuiChip";
 import { MuiTooltip } from "./components/MuiTooltip";
 import { MuiTable } from "./components/MuiTable";
+import { MuiAlert } from "./components/MuiAlert";
 
 
 function App() {
@@ -59,7 +60,8 @@ function App() {
       {/* <MuiList /> */}
       {/* <MuiChip /> */}
       {/* <MuiTooltip /> */}
-      <MuiTable />
+      {/* <MuiTable /> */}
+      <MuiAlert />
 
     </div>
   );
